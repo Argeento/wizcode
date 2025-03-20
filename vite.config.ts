1bf4ea152fe193542aee4a0267c4b2e75a7aadba
Adrian Wieprzkowicz <adrian.wieprzkowicz@gmail.com>
2025-03-20 22:21:26 +0100

    Add auto imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [
+    vue(),
+    tailwindcss(),
+    Components(),
+    AutoImport({
+      imports: ['vue'],
+    }),
+  ],
 })
